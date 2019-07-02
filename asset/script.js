82656bcaaf99561f3cc6abf152643316772e1b3a
Ziphil <yfe23457@nifty.com>
2019-07-02 23:56:18 +0900

    めためた

--- asset/script.js
+++ asset/script.js
@@ -3,12 +3,14 @@
 
 const BOX_SIZE = 500;
 const BOX_WIRE_THICKNESS = 5;
-const BALL_SIZE = 75;
+const BALL_SIZE = 80;
+const BALL_WIRE_THICKNESS = 30;
 const BALL_COUNT = 15;
 
 const BOX_WIRE_COLOR = "#555555";
 
-const ROTATE_SPEED = {x: -0.003, y: 0.002, z: 0.001};
+const BOX_ROTATE_SPEED = {x: -0.003, y: 0.002, z: 0.001};
+const BALL_ROTATE_SPEED = {x: 0.003, y: -0.002, z: -0.001};
 
 const BOX_ELEMENT = document.querySelector(".box");
 const BALL_ELEMENT = document.querySelector(".ball");
@@ -16,10 +18,6 @@ const BALL_ELEMENT = document.querySelector(".ball");
 const BALLS = [];
 const BOX_ROOT = new Zdog.Anchor({});
 const BALL_ROOT = new Zdog.Anchor({});
-
-function init() {
-  Zfont.init(Zdog);
-}
 
 function createBox() {
   let box = new Zdog.Anchor({
@@ -51,25 +49,34 @@ function createBox() {
 }
 
 function createBalls() {
-  let go = (translate, hue) => {
+  let go = (translate, rotate, hue) => {
     let scaledTranslate = objectMap(translate, (value) => value * BOX_SIZE / 2);
-    let ball = new Zdog.Shape({
+    let ball = new Zdog.Ellipse({
       addTo: BALL_ROOT,
-      stroke: BALL_SIZE,
-      path: [{x: 0, y: 0, z: 0}],
+      diameter: BALL_SIZE,
+      stroke: BALL_WIRE_THICKNESS,
       translate: scaledTranslate,
+      rotate: rotate,
       color: "hsl(" + Math.round(hue) + ", 60%, 50%)"
     });
     ball.translate.vx = scaledTranslate.vx;
     ball.translate.vy = scaledTranslate.vy;
     ball.translate.vz = scaledTranslate.vz;
+    ball.rotate.vx = rotate.vx;
+    ball.rotate.vy = rotate.vy;
+    ball.rotate.vz = rotate.vz;
     return ball;
   }
+  let baseHue = Math.random() * 360;
   for (let i = 0 ; i < BALL_COUNT ; i ++) {
     let position = objectMap({x: 0, y: 0, z: 0}, (value) => Math.random() * 2 - 1);
     let velocity = objectMap({vx: 0, vy: 0, vz: 0}, (value) => Math.random() * 0.02 - 0.01);
-    let hue = Math.random() * 120 + 120;
-    let ball = go(Object.assign(position, velocity), hue);
+    let rotate = objectMap({vx: 0, vy: 0, vz: 0}, (value) => Math.random() * 0.04 - 0.02);
+    let hue = Math.random() * 60 + baseHue;
+    if (Math.random() < 0.05) {
+      hue += 180;
+    }
+    let ball = go(Object.assign(position, velocity), rotate, hue % 360);
     BALLS.push(ball);
   }
 }
@@ -93,9 +100,9 @@ function render() {
 
 function updateBox() {
   for (let root of [BOX_ROOT, BALL_ROOT]) {
-    root.rotate.x += ROTATE_SPEED.x;
-    root.rotate.y += ROTATE_SPEED.y;
-    root.rotate.z += ROTATE_SPEED.z;
+    root.rotate.x += BOX_ROTATE_SPEED.x;
+    root.rotate.y += BOX_ROTATE_SPEED.y;
+    root.rotate.z += BOX_ROTATE_SPEED.z;
     root.updateGraph();
   }
 }
@@ -104,9 +111,13 @@ function updateBalls() {
   for (let ball of BALLS) {
     let limit = BOX_SIZE / 2 - BALL_SIZE / 2;
     let translate = ball.translate;
+    let rotate = ball.rotate;
     translate.x += translate.vx;
     translate.y += translate.vy;
     translate.z += translate.vz;
+    rotate.x += rotate.vx;
+    rotate.y += rotate.vy;
+    rotate.z += rotate.vz;
     if (translate.x >= limit) {
       translate.x = limit;
       translate.vx = -translate.vx;
@@ -154,6 +165,5 @@ function objectMap(object, operation) {
   return newObject;
 }
 
-init();
 create();
 animate();
